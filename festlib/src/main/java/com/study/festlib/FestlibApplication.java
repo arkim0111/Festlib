afa1e4e58c45612c437b17790bbfcef16c048dee
arkim0111 <aramingk95@gmail.com>
2023-02-05 22:38:46 +0900

    commit test

---
+++ festlib/src/main/java/com/study/festlib/FestlibApplication.java
@@ -0,0 +1,13 @@
+package com.study.festlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestlibApplication.class, args);
+	}
+
+}
