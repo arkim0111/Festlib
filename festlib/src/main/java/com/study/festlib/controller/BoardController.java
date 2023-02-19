695c06ce3a3b0d281035b04f731f9367d0cddebd
LSA00 <darkcat90@naver.com>
2023-02-19 14:16:51 +0900

    Board 수정

--- festlib/src/main/java/com/study/festlib/controller/BoardController.java
+++ festlib/src/main/java/com/study/festlib/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.study.festlib.controller;
 
+import com.study.festlib.domain.Board;
 import com.study.festlib.service.BoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RequestMapping("/Festlib")
 @RestController
@@ -17,8 +20,8 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/board")
-    public String Board(Model model) {
-        model.addAttribute("boardList", boardService.boardList());
-        return "board";
+    public List<Board> Board() {
+        List<Board> boardList = boardService.boardList();
+        return boardList;
     }
 }
