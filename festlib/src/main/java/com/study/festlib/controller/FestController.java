d4d7e0baab064027b17e335f811ca42de6a89cb6
LSA00 <darkcat90@naver.com>
2023-02-26 18:10:32 +0900

    Festival 요청기능

--- festlib/src/main/java/com/study/festlib/controller/FestController.java
+++ festlib/src/main/java/com/study/festlib/controller/FestController.java
@@ -1,11 +1,12 @@
 package com.study.festlib.controller;
 
 import com.study.festlib.domain.Festival;
-import com.study.festlib.repository.FestRepository;
 import com.study.festlib.service.FestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequestMapping("/Festlib")
@@ -15,11 +16,19 @@ public class FestController {
     @Autowired
     FestService festService;
 
+    //상세 페이지
     @GetMapping(value = "/fest/{fno}")
     public Festival festDetail(@PathVariable("fno") String fno){
         Festival festival = festService.selectOneFest(fno);
 
         return festival;
+    }
+
+    //페스티벌 요청
+    @ResponseBody
+    @PostMapping(value="/fest/request")
+    public Festival requestFest (Festival festival){
+        return festService.requestFest(festival);
     }
 
 }
