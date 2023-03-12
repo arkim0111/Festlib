0da779bb86caaffe66ba7613b488c811a82796d2
LSA00 <darkcat90@naver.com>
2023-03-12 18:31:18 +0900

    Member repository, mapper 까지 test전

--- festlib/src/main/java/com/study/festlib/repository/MemberRepository.java
+++ festlib/src/main/java/com/study/festlib/repository/MemberRepository.java
@@ -1,11 +1,24 @@
 package com.study.festlib.repository;
 
+import com.study.festlib.domain.Member;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemberRepository {
 
-    //회원가입
+    public Member selectOneMem(String MEM_ID);
 
-    //로그인
+    //회원정보 insert
+    public void insertOneMem(Member member);
+
+    //회원수정 update
+    public void updateOneMem(Member member);
+
+    //회원정지 or 탈퇴 update
+    public void deleteOneMem(Integer MEM_IDX);
+
+    //회원 리스트
+    public List<Member> selectMemList();
 }
