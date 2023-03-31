b5506ac07c3ec71bd2a0b48ca9824498be62ca7f
arkim0111 <aramingk95@gmail.com>
2023-03-31 22:08:19 +0900

    REPLY SQL & REPLY DOMAIN 수정

--- festlib/src/main/java/com/study/festlib/domain/Reply.java
+++ festlib/src/main/java/com/study/festlib/domain/Reply.java
@@ -11,11 +11,11 @@ import java.util.Date;
 @AllArgsConstructor
 public class Reply {
 
-    private Long reply_board_idx; // 게시글 번호
-    private Long reply_idx; // 댓글 번호
-    private String reply_mem_id; // 댓글 작성자 ID
-    private String reply_content; // 댓글 내용
-    private Date reply_date; // 댓글 작성 날짜
+    private Long REPLY_BOARD_IDX; // 게시글 번호
+    private Long REPLY_IDX; // 댓글 번호
+    private String REPLY_MEM_ID; // 댓글 작성자 ID
+    private String REPLY_CONTENT; // 댓글 내용
+    private Date REPLY_DATE; // 댓글 작성 날짜
 
 
 }
